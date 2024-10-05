2964f3b371e87bf2c5a7b494c2f7bcf863af5b03
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-05 10:11:43 -0300

    locais.c atualizado
    troquei o struct de locais por um struct de objetos. todo objeto tem um ponteiro que aponta para o local onde ele se encontra. locais possuem ponteiro NULL, pra diferenciar um do outro

--- UFPB_Simulator/locais.c
+++ UFPB_Simulator/locais.c
@@ -1,48 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
-
-typedef struct {
-    char* descricao;  // descrição que aparecerá ao olhar ao redor
-    char* rotulo; // nome do local
-} tlocal;
-
-tlocal locais[] = {
-    {"Um prédio generico com vasta concentração de distúrbios mentais\n", "ao Centro de informatica"},
-    {"Um prédio alto com sérios problemas estruturais.\n uma nevóa densa cobre os corredores deste local... assustador\n", "ao Liepe"},
-    {"Uma estrutura organizada feita de tijolos, com vários gatos ao redor.\n O único restaurante do campus se encontra aqui\n", "ao CTDR"},
-    {"Uma lagoa de agua parada em uma clareira escondida nos matos.\n Voce sente uma vontade primitiva de jogar pedras lá dentro.\n", "ao poco da AIDS"},
-    {"Um banheiro em que todas as privadas não tem assento.\n Pelo menos tem papel higienico...\n", "ao banheiro"}
-}; // struct com todos os locais acessiveis do jogo
-
-#define num_locais (sizeof(locais) / sizeof(*locais))
-
-static unsigned int local_jogador = 0;
+#include "objetos.h"
+#include "misc.h"
+#include "substantivo.h"
 
 void exec_olhar(const char* substantivo) {
     setlocale(LC_ALL, " ");
-    if(substantivo != NULL && strcasecmp(substantivo, "ao redor") == 0) { //verifica se o usuário quer olhar ao redor
-        printf("Você está em %s", locais[local_jogador].descricao);
+    if(substantivo != NULL && strcasecmp(substantivo, "ao redor") == 0) { //verifica se o usuÃ¡rio quer olhar ao redor
+        printf("VocÃª estÃ¡ em %s", player->local->descricao);
+        lista_objetos_presentes(player->local);
     } else {
-        printf("Eu não sei oque voce quer que eu veja.\n");
+        printf("Eu nÃ£o sei oque voce quer que eu veja.\n");
     }
 }
 
 void exec_ir(const char* substantivo) {
     setlocale(LC_ALL, " ");
-    unsigned int i = 0;
+    tObjetos *obj = visivel_existe("Onde voce quer ir ", substantivo);
 
-    for(i=0; i<num_locais; i++) {
-            if(substantivo != NULL && strcasecmp(substantivo, locais[i].rotulo) == 0) {
-                if(i == local_jogador) {
-                    printf("voce já está nesse local.\n");
-                } else {
-                    printf("Ok!\n");
-                    local_jogador = i;
-                    exec_olhar("ao redor");
-                }
-                return;
-            }
+    if(obj == NULL){
+
+    } else if(obj->local == NULL && obj != player->local){
+        printf("OK.\n");
+        player->local = obj;
+      exec_olhar("ao redor");
+    } else {
+        printf("Impossivel se aproximar mais que isso.\n");
     }
-    printf("Eu não conheço esse lugar.\n");
 }
