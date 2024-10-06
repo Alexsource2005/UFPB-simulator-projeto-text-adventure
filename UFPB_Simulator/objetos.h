bd68f9cae3faa09f6acece5e276195217961a62e
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-06 13:07:29 -0300

    novas passagens
    Adicionei o atributo "destino" ao struct de objetos. e atualizei a lista de define

--- UFPB_Simulator/objetos.h
+++ UFPB_Simulator/objetos.h
@@ -2,6 +2,7 @@ typedef struct objeto {
     const char *descricao;
     const char *rotulo;
     struct objeto *local; //ponteiro para o local onde o objeto se encontra
+    struct objeto *destino; // exclusivo das passagens de local a local
 } tObjetos;
 
 extern tObjetos lista_objetos[];
@@ -17,4 +18,14 @@ extern tObjetos lista_objetos[];
 #define notebook (lista_objetos + 8)
 #define player (lista_objetos + 9)
 
-#define fim_lista (lista_objetos + 10) // aumentar o número conforme coloca mais objetos
+// passagens
+#define into_liepe (lista_objetos + 10)
+#define exit_lipe (lista_objetos + 11)
+#define into_ctdr (lista_objetos + 12)
+#define exit_ctdr (lista_objetos + 13)
+#define into_aids (lista_objetos + 14)
+#define exit_aids (lista_objetos + 15)
+#define into_banheiro (lista_objetos + 16)
+#define exit_banheiro (lista_objetos + 17)
+
+#define fim_lista (lista_objetos + 18) // aumentar o nÃºmero conforme coloca mais objetos
