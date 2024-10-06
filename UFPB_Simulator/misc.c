5ce78355d2429c9adc9a4d5c592d45791b9af314
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-06 09:32:32 -0300

    comandos gerais
    uma função para listar certos tipos de objetos. e uma função que retorna se existe algum NPC no local com o jogador

--- UFPB_Simulator/misc.c
+++ UFPB_Simulator/misc.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 #include "objetos.h"
 
-int lista_objetos_presentes(tObjetos *local_atual) {
-    int cont = 0;
+tObjetos *ator_aqui(void) {
     tObjetos *obj;
     for(obj = lista_objetos; obj < fim_lista; obj++){
-        if(obj != player && obj->local == local_atual){ //exclui o objeto "jogador"
-            if(cont++ == 0)
-                printf("Você vê: \n");
-            printf("- %s \n", obj->descricao);
+        if(obj->local == player->local && obj == jovem)
+            return obj;
+    }
+    return NULL;
+}
+
+
+void imprimir_objetos(tObjetos *obj, int *cont, const char *mensagem) {
+    if (*cont == 0) {
+        printf("%s\n", mensagem);
+    }
+    printf("- %s\n", obj->descricao);
+    (*cont)++;
+}
+
+
+int lista_objetos_presentes(tObjetos *local_atual) {
+    int cont = 0;
+
+    if (local_atual != player && local_atual != NULL) {
+        for (tObjetos *obj = lista_objetos; obj < fim_lista; obj++) {
+            if (obj != player && obj->local == local_atual) {
+                imprimir_objetos(obj, &cont, "VocÃª vÃª:");
+            }
+        }
+    } else if (local_atual == NULL) {
+        for (tObjetos *obj = lista_objetos; obj < fim_lista; obj++) {
+            if (obj != player && obj->local == NULL) {
+                imprimir_objetos(obj, &cont, "Locais acessiveis:");
+            }
+        }
+    } else {
+        for (tObjetos *obj = lista_objetos; obj < fim_lista; obj++) {
+            if (obj != player && obj->local == local_atual) {
+                imprimir_objetos(obj, &cont, "VocÃª possui em seus bolsos:");
+            }
         }
     }
-    return cont; // retorna o número de objetos no local
+
+    return cont; // retorna o nÃºmero de objetos no local
 }
