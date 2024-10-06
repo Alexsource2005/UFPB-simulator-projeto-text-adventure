0e4eb0d710d58142d36cd6cb712e3e2a2bac087b
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-06 10:06:29 -0300

    nada dms

--- UFPB_Simulator/inventory.c
+++ UFPB_Simulator/inventory.c
@@ -5,29 +5,29 @@
 #include "movimento.h"
 
 void exec_pegar(char *substantivo){
-    tObjetos *obj = visivel_existe("Oque voce quer pegar", substantivo); //verificar se o objeto existe no sistema e se È visivel ao jogador
+    tObjetos *obj = visivel_existe("Oque voce quer pegar", substantivo); //verificar se o objeto existe no sistema e se √© visivel ao jogador
 
     if(obj == NULL){
 
     } else if(obj == player){
-        printf("N„o faÁa isso consigo mesmo.\n");
+        printf("N√£o fa√ßa isso consigo mesmo.\n");
 
     } else if(obj->local == player){
-        printf("VocÍ j· est· em posse desse objeto.\n");
+        printf("Voc√™ j√° est√° em posse desse objeto.\n");
 
     } else if(obj->local == jovem) {
-        printf("VocÍ deveria falar %s antes de pegar algo que n„o te pertence.", obj->local->rotulo);
+        printf("Voc√™ deveria falar %s antes de pegar algo que n√£o te pertence.", obj->local->rotulo);
 
     } else {
-        mover_objeto(obj, player); //implementar funÁ„o de mudar local do objeto
+        mover_objeto(obj, player); //implementar fun√ß√£o de mudar local do objeto
     }
 }
 
 void exec_jogar(char *substantivo){
     if(player->local == AIDS)
-        mover_objeto(pega_possesao(player, "jogar", substantivo), AIDS); //em teoria isso È sÛ pra jogar pedras na fossa
+        mover_objeto(pega_possesao(player, "jogar", substantivo), AIDS); //em teoria isso √© s√≥ pra jogar pedras na fossa
     else
-        printf("N„o tem local adequado para jogar esse objeto.\n");
+        printf("N√£o tem local adequado para jogar esse objeto.\n");
 }
 
 
@@ -40,13 +40,13 @@ void exec_dar(char *substantivo) {
 }
 
 void exec_checar(char *substantivo){
-    if(strcasecmp(substantivo, "o inventario") == 0){
+    if(strcasecmp(substantivo, "inventario") == 0){
         if(lista_objetos_presentes(player) == 0)
-            printf("Voce n„o tem nada nos bolsos.\n");
+            printf("Voce n√£o tem nada nos bolsos.\n");
         else
             lista_objetos_presentes(player);
 
-    } else if(strcasecmp(substantivo, "o mapa") == 0) {
+    } else if(strcasecmp(substantivo, "mapa") == 0) {
         lista_objetos_presentes(NULL);
 
     } else {
