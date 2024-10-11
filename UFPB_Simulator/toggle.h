f10eca4c1a308d3b68754beb2fee449c0b4a84de
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-11 19:01:34 -0300

    Continuação do lance de abrir e fechar (arquivo auxiliar)
    Denovo, todo um paranauê só pra abrir uma maldita porta

---
+++ UFPB_Simulator/toggle.h
@@ -0,0 +1,10 @@
+extern void nao_pode_ser_aberto(void);
+extern void nao_pode_ser_fechado(void);
+
+extern void ja_esta_aberto(void);
+extern void ja_esta_fechado(void);
+
+extern void ainda_esta_aberto(void);
+
+extern void troca_porta_banheiro(void);
+extern void troca_porta_liepe(void);
