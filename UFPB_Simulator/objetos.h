84245a4f1b07156bb662e71d83cc3c19b5693fd0
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-11 14:09:55 -0300

    Novos atributos para cada objeto
    o gerador automatico serve justamente pra não ter que colocar todos esses atributos a mão

--- UFPB_Simulator/objetos.h
+++ UFPB_Simulator/objetos.h
@@ -1,38 +1,43 @@
+
 typedef struct objeto {
     const char *descricao;
-    const char **rotulo; // agora será uma lista de rótulos possiveis para cada objeto
+    const char **rotulos; // agora será uma lista de rótulos possiveis para cada objeto
     struct objeto *local; //ponteiro para o local onde o objeto se encontra
     struct objeto *destino; // exclusivo das passagens de local a local
+    struct objeto *aparente; // exclusivo para passagens que levam a locais inesperados ou que serão bloqueados
+    const char *detalhes; // detalhes que aparecerão ao examinar um objeto de perto
+    const char *conteudo; // para objetos que irão conter outro objeto
+    const char *texto_GO; //mensagem fofa única para cada local
+    int peso; // peso de cada objeto
+    int capacidade; // capacidade de carga de cada ator
+    int vida; //não sei se será necessário, mas bora
+
 } tObjetos;
 
 extern tObjetos lista_objetos[];
 
-#define CI (lista_objetos + 0)
-#define Liepe (lista_objetos + 1)
-#define CTDR (lista_objetos + 2)
-#define AIDS (lista_objetos + 3)
-#define banheiro (lista_objetos + 4)
-#define pvc (lista_objetos + 5)
-#define jovem (lista_objetos + 6)
-#define arduino (lista_objetos + 7)
-#define notebook (lista_objetos + 8)
-#define player (lista_objetos + 9)
+#define CI	(lista_objetos + 0)
+#define Liepe	(lista_objetos + 1)
+#define CTDR	(lista_objetos + 2)
+#define AIDS	(lista_objetos + 3)
+#define banheiro	(lista_objetos + 4)
+#define pvc	(lista_objetos + 5)
+#define jovem	(lista_objetos + 6)
+#define arduino	(lista_objetos + 7)
+#define notebook	(lista_objetos + 8)
+#define player	(lista_objetos + 9)
+#define into_liepe	(lista_objetos + 10)
+#define exit_liepe	(lista_objetos + 11)
+#define into_ctdr	(lista_objetos + 12)
+#define exit_ctdr	(lista_objetos + 13)
+#define into_aids	(lista_objetos + 14)
+#define exit_aids	(lista_objetos + 15)
+#define into_banheiro	(lista_objetos + 16)
+#define exit_banheiro	(lista_objetos + 17)
+#define block_ci	(lista_objetos + 18)
+#define block_liepe	(lista_objetos + 19)
+#define block_ctdr	(lista_objetos + 20)
+#define block_aids	(lista_objetos + 21)
+#define block_banheiro	(lista_objetos + 22)
 
-// passagens
-#define into_liepe (lista_objetos + 10)
-#define exit_liepe (lista_objetos + 11)
-#define into_ctdr (lista_objetos + 12)
-#define exit_ctdr (lista_objetos + 13)
-#define into_aids (lista_objetos + 14)
-#define exit_aids (lista_objetos + 15)
-#define into_banheiro (lista_objetos + 16)
-#define exit_banheiro (lista_objetos + 17)
-
-// "paredes" que impedem o jogador de continuar além do esperado
-#define block_ci (lista_objetos + 18)
-#define block_liepe (lista_objetos + 19)
-#define block_ctdr (lista_objetos + 20)
-#define block_aids (lista_objetos + 21)
-#define block_banheiro (lista_objetos + 22)
-
-#define fim_lista (lista_objetos + 23) // aumentar o número conforme coloca mais objetos
+#define fim_lista	(lista_objetos + 23)
