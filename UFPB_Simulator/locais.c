afea8f89496dc4f7d1a9c2020ca43222cf5dd2ba
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-11 14:13:34 -0300

    utilização dos atributos "detalhes" e "texto_GO"
    "detalhes" permite exibir uma descrição mais detalhada de um objeto especifico depois de usar o comando "olhar" e o atributo "texto_GO" serve pra dar uma descrição detalhada quando voce utiliza uma passagem.

--- UFPB_Simulator/locais.c
+++ UFPB_Simulator/locais.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
@@ -11,30 +12,60 @@ void exec_olhar(const char* substantivo) {
         printf("Você está em %s", player->local->descricao);
         lista_objetos_presentes(player->local);
     } else {
-        printf("Eu não sei oque voce quer que eu veja.\n");
+        tObjetos *obj = visivel_existe("Oque voce quer olhar", substantivo);
+
+        switch(pega_distancia(player, obj)){
+        case dist_AquiContido:
+            printf("Dificil de ver, tente pegar primeiro.\n");
+            break;
+
+        case dist_AliDoLado:
+            printf("Muito longe. Se aproxime, por favor. \n");
+            break;
+
+        case dist_NaoAqui:
+            printf("Você não vê nenhum %s aqui.\n", substantivo);
+            break;
+
+        case dist_objetoNaoReconhecido:
+            // já vai ser resolvido pela função "visivel_existe"
+            break;
+
+        default:
+            printf("%s\n", obj->detalhes);
+            lista_objetos_presentes(obj);
+
+        }
+    }
+}
+
+static void mover_jogador(tObjetos *passagem){
+    printf("%s\n", passagem->texto_GO);
+    if(passagem->destino != NULL) {
+        player->local = passagem->destino;
+        printf("\n");
+        exec_olhar("ao redor");
     }
 }
 
 void exec_ir(const char* substantivo) {
     setlocale(LC_ALL, " ");
-    tObjetos *obj = visivel_existe("Onde voce quer ir ", substantivo);
+    tObjetos *obj = visivel_existe("Onde voce quer ir ", substantivo); //tentar colocar um while aqui pra testar
 
-    if(obj == NULL){
+    switch(pega_distancia(player, obj)){
+    case dist_AliDoLado:
+        mover_jogador(pega_passagem(player->local, obj));
+        break;
 
-    } else if(pega_passagem(player->local, obj) != NULL){
-        printf("OK.\n");
-      player->local = obj;
-      exec_olhar("ao redor");
-
-    } else if(obj->local != player->local){
+    case dist_NaoAqui:
         printf("Voce não vê nenhum %s aqui.\n", substantivo);
+        break;
 
-    } else if(obj->descricao != NULL){
-        printf("OK.\n");
-        player->local = obj->destino;
-        exec_olhar("ao redor");
+    case dist_objetoNaoReconhecido:
+        // a função visivel_existe já lida com esse caso
+        break;
 
-    } else {
-        printf("Impossivel se aproximar mais que isso.\n");
+    default:
+        mover_jogador(obj);
     }
 }
