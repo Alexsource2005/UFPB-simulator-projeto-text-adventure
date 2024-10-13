ee6a482117845fd6a03ae2e398a82fb989a7329d
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-12 22:27:54 -0300

    Update abrir_fechar.c

--- UFPB_Simulator/abrir_fechar.c
+++ UFPB_Simulator/abrir_fechar.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include "objetos.h"
 #include "alcance.h"
+#include "comparar.h"
 
 bool exec_abrir(void) {
     tObjetos *obj = objeto_no_alcance("oque voce quer abrir", parametros[0]);
