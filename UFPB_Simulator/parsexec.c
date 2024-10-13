639eb58b090603245222293b482106aea57bc612
Alexander Fernandes <alessandrafernandes100922@gmail.com>
2024-10-12 22:36:07 -0300

    atualização do módulo "compara_executa"

--- UFPB_Simulator/parsexec.c
+++ UFPB_Simulator/parsexec.c
@@ -1,95 +1,55 @@
-#include <stdbool.h>
+#include <ctype.h> //usaremos a função "isspace()" para fazer um teste mais apurado. isspace() retorna 0 se o caractere passado NÃO FOR um espaço vazio
 #include <stdio.h>
-#include <string.h>
-#include <locale.h>
+#include <stdbool.h>
+#include "objetos.h"
+#include "misc.h"
+#include "comparar.h" //implementar
 #include "locais.h"
 #include "inventory.h"
 #include "abrir_fechar.h"
 
+typedef struct {
+    const char *padrao;
+    bool (*funcao)(void);
 
-bool comparaeExecuta(char *comando) {
-    if (comando == NULL || strlen(comando) == 0) {
-        printf("Comando vazio.\n");
-        return true; // Continue o loop
-    }
+} tComando; // vai servir pra ligar um padrão de comando com uma função de execução especifica
 
-    setlocale(LC_ALL, " "); // Mantenha isso no início da função ou no main
+static bool exec_sair(void){
+    return false;
+}
 
-    char *verbo = strtok(comando, " \n"); // Obtém o verbo
-    char *substantivo = strtok(NULL, "\n"); // Obtém o substantivo
+static bool exec_sem_padrao(void){
+    const char *source = *parametros; // "parametros" vai ser uma variavel global
+    int len;
 
-    if (verbo != NULL) {
-        if (strcasecmp(verbo, "sair") == 0) {
-            return false; // Sair do jogo
+    for(len = 0; source[len] != '\0' && !isspace(source[len]); len++); //calcula o tamanho da primeira palavra escrita pelo jogador (verbo)
+    if(len > 0)
+        printf("Eu não sei como %.*s.", len, source); //%.*s permite que antes do parametro de string coloquemos um número inteiro que limite o número máxximo de caracteres
+    return true;
+}
 
-        } else if (strcasecmp(verbo, "olhar") == 0) {
-            exec_olhar(substantivo);
+bool comparaeExecuta(char *comando){
+    static const tComando lista_comando[] = //lista de padrões, para adicionar sinonimos, só adicionar e ligar a mesma função já existente
+    {
+        {"sair", exec_sair},
+        {"olhar", exec_olhar_aoredor},
+        {"olhar ao redor", exec_olhar_aoredor},
+        {"olhar para A", exec_olhar},
+        {"olhar A", exec_olhar},
+        {"examinar A", exec_olhar},
+        {"ir para A", exec_ir},
+        {"ir A", exec_ir},
+        {"jogar A", exec_jogar},
+        {"pedir A", exec_pedir},
+        {"dar A", exec_dar},
+        {"checar inventario", exec_inventario},
+        {"inventario", exec_inventario},
+        {"abrir A", exec_abrir},
+        {"fechar A", exec_fechar},
+        {"A", exec_sem_padrao}
+    };
 
-        } else if (strcasecmp(verbo, "ir") == 0) {
-            if (substantivo != NULL) {
-                exec_ir(substantivo);
-            } else {
-                printf("Para onde você quer ir?\n");
-            }
-
-
-        } else if (strcasecmp(verbo, "pegar") == 0) {
-            if (substantivo != NULL) {
-                exec_pegar(substantivo);
-            } else {
-                printf("O que você quer pegar?\n");
-            }
-
-
-        } else if (strcasecmp(verbo, "jogar") == 0) {
-            if (substantivo != NULL) {
-                exec_jogar(substantivo);
-            } else {
-                printf("O que você quer jogar?\n");
-            }
-
-
-        } else if (strcasecmp(verbo, "dar") == 0) {
-            if (substantivo != NULL) {
-                exec_dar(substantivo);
-            } else {
-                printf("O que você quer dar?\n");
-            }
-
-
-        } else if (strcasecmp(verbo, "pedir") == 0) {
-            if (substantivo != NULL) {
-                exec_pedir(substantivo);
-            } else {
-                printf("O que você quer pedir?\n");
-            }
-
-
-        } else if (strcasecmp(verbo, "checar") == 0) {
-            if (substantivo != NULL) {
-                exec_checar(substantivo);
-            } else {
-                printf("O que você quer checar?\n");
-            }
-
-        } else if(strcasecmp(verbo, "abrir") == 0){
-            if(substantivo != NULL) {
-                exec_abrir(substantivo);
-            } else {
-                printf("O que você quer tentar abrir?\n");
-            }
-
-        } else if(strcasecmp(verbo, "fechar") == 0) {
-            if(substantivo != NULL) {
-                exec_fechar(substantivo);
-            } else {
-                printf("O que você quer tentar fechar?");
-            }
-
-        } else {
-            printf("Eu não sei '%s'.\n", verbo);
-        }
-    }
-
-    return true; // Continua o loop do jogo
+    const tComando *cmd;
+    for(cmd = lista_comando; !compara_comando(comando, cmd->padrao); cmd++);
+    return(*cmd->funcao)();
 }
